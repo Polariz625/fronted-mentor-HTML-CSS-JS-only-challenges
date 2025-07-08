98ece8ba9c38f7a01abc14d09fef2fe1a37403c1
Polariz625 <braianlopc@gmail.com>
2025-07-08 14:14:49 -0500

    fixing share icon button behavior

--- article-preview-component-master/js/main.js
+++ article-preview-component-master/js/main.js
@@ -5,7 +5,8 @@ const shareBtn = d.querySelector(".share-btn-3");
 d.addEventListener("click", (e) => {
   if (e.target.matches(".share-btn-1") || e.target.matches(".share-btn-1 *")) {
     shareSocials.classList.toggle("hidden");
-
+    shareBtn.classList.add("active-btn");
+    
     // shareSocials.classList.remove("hidden");
     // shareSocials.classList.add("move-right-left");
     // shareSocials.classList.remove("move-left-right");
@@ -13,7 +14,8 @@ d.addEventListener("click", (e) => {
 
   if (e.target.matches(".share-btn-2") || e.target.matches(".share-btn-2 *")) {
     shareSocials.classList.toggle("hidden");
-    shareBtn.classList.toggle("active-btn");
+    shareBtn.classList.remove("active-btn");
+
     // shareSocials.classList.remove("move-right-left");
     // shareSocials.classList.add("move-left-right");
 
