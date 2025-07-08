22c28094738ab70bce439f8bc729e3f9c4c9ba1e
Polariz625 <braianlopc@gmail.com>
2025-07-08 13:36:12 -0500

    adding new fronted mentor challenge to the repo

---
+++ article-preview-component-master/js/main.js
@@ -0,0 +1,27 @@
+const d = document;
+const shareSocials = d.querySelector(".share-container");
+const shareBtn = d.querySelector(".share-btn-3");
+
+d.addEventListener("click", (e) => {
+  if (e.target.matches(".share-btn-1") || e.target.matches(".share-btn-1 *")) {
+    shareSocials.classList.toggle("hidden");
+
+    // shareSocials.classList.remove("hidden");
+    // shareSocials.classList.add("move-right-left");
+    // shareSocials.classList.remove("move-left-right");
+  }
+
+  if (e.target.matches(".share-btn-2") || e.target.matches(".share-btn-2 *")) {
+    shareSocials.classList.toggle("hidden");
+    shareBtn.classList.toggle("active-btn");
+    // shareSocials.classList.remove("move-right-left");
+    // shareSocials.classList.add("move-left-right");
+
+  }
+
+  if (e.target.matches(".share-btn-3") || e.target.matches(".share-btn-3 *")) {
+    shareSocials.classList.toggle("hidden");
+    shareBtn.classList.toggle("active-btn");
+  }
+
+});
